176025b49f9cee9dca2bd75425aa46348abc57a5
Chanchal pareek <chanchal.pareek11@gmail.com>
2022-04-02 19:40:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running on cp http://localhost:${port}/`);
+  console.log(`Server running on cpp http://localhost:${port}/`);
 });
